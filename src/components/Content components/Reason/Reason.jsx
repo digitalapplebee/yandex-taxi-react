f02d6b8c1f7f83cb9bdf8090556230387f866834
digitalapplebee <digitalapplebee@gmail.com>
2018-09-11 20:15:44 +0300

    Some edits

--- src/components/Content components/Reason/Reason.jsx
+++ src/components/Content components/Reason/Reason.jsx
@@ -13,28 +13,28 @@ export default class Reason extends React.Component {
       <div className="reason-container">
         <p className="reason-text-label">Почему Яндекс.Такси?</p>
         <div className="reason-grid">
-          <div>
+          <div className="reason-block">
             <div className="reason-style-center">
               <MdWallpaper className="reason-icon"/> 
             </div>
             <p className="reason-text-grid-label">Навигация</p>
             <p className="reason-text-description">В программе имеется встроенный "Яндекс.Навигатор", который помогает водителям быстрее добраться до цели.</p>
           </div>
-          <div>
+          <div className="reason-block">
             <div className="reason-style-center">
               <IoIosPeople className="reason-icon"/> 
             </div>
             <p className="reason-text-grid-label">Посадка</p>
             <p className="reason-text-description">У Вас не возникнет проблем с поиском и посадкой пассажиров, так как встроенный навигатор приведёт вас к месту посадки пассажира.</p>
           </div>
-          <div>
+          <div className="reason-block">
             <div className="reason-style-center">
               <IoIosAirplane className="reason-icon" /> 
             </div>
             <p className="reason-text-grid-label">Трансферы</p>
             <p className="reason-text-description">Регулярные трансферы в Национальный аэропорт «Минск», а так же из Национальный аэропорт «Минск» в город.</p>
           </div>
-          <div>
+          <div className="reason-block">
             <div className="reason-style-center">
               <IoMdDocument className="reason-icon" /> 
             </div>
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -11,14 +11,18 @@ export default class Content extends React.Component {
   render() {
     return (
       <div className="content-container">
+
         <p className="content-text">Приглашаем водителей на автомобили организации для работы в Яндекс.Такси</p>
+
         <div className="content-submit-form">
           <input type="text" placeholder="Ваше имя или фамилия"/>
           <input type="text" placeholder="Номер телефона"/>
         </div>
+
         <div className="content-button-center">
           <button className="content-button-submit">Отправить заявку</button>
         </div>
+        
         <p className="content-text-calling">или звоните</p>
         <p className="content-text-number">+375 (44) 588-50-00</p>
         <img className="content-img-car" src={car} alt="Yandex Car"/>
