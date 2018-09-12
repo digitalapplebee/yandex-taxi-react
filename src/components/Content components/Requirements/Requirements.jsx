ea02b8fa26a743c95cdfc9bed06ffe31fca9fee5
digitalapplebee <digitalapplebee@gmail.com>
2018-09-12 22:43:40 +0300

    Edit Requirements block

--- src/components/Content components/Requirements/Requirements.jsx
+++ src/components/Content components/Requirements/Requirements.jsx
@@ -2,20 +2,28 @@ import React from 'react'
 
 import './requirements.css'
 
+import Car2 from '../../../img/car-2.png'
+import {FaCheck} from 'react-icons/fa'
+
 export default class Requirements extends React.Component {
-  render(){
-    return(
+  render() {
+    return (
       <div className="requirements-container">
-        <p>Условия паботы и требования</p>
-        <ul>
-          <li>Возраст от 21 года</li>
-          <li>Стаж В/У от 3 лет</li>
-          <li>Страховка пассажиров</li>
-          <li>Автомобиль от 2003 года</li>
-          <li>Смартфон на Android</li>
-        </ul>
-        <button>Стать водителем</button>
-        {/* img */}
+        <div>
+          <p className="requirements-label">Условия паботы и требования</p>
+          <div className="requirements-list-block">
+            <ul>
+              <li><FaCheck className="requirements-list-check-icon"/>Возраст от 21 года</li>
+              <li><FaCheck className="requirements-list-check-icon"/>Стаж В/У от 3 лет</li>
+              <li><FaCheck className="requirements-list-check-icon"/>Страховка пассажиров</li>
+              <li><FaCheck className="requirements-list-check-icon"/>Автомобиль от 2003 года</li>
+              <li><FaCheck className="requirements-list-check-icon"/>Смартфон на Android</li>
+            </ul>
+            <button className="requirements-button">Стать водителем</button>
+          </div>
+        </div>
+
+        <img src={Car2} alt="car2" />
       </div>
     )
   }
