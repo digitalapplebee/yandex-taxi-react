fc45793d72926e89f0f77063269e2882a1c4a4e4
digitalapplebee <digitalapplebee@gmail.com>
2018-09-23 02:56:00 +0300

    Add scrolling header event

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,14 +7,40 @@ import './header.css'
 import { FaVk, FaFacebookF, FaTwitter } from 'react-icons/fa'
 
 export default class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      scrolling: false,
+    }
+    this.handleScroll = this.handleScroll.bind(this)
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(event) {
+    if (window.scrollY === 0 && this.state.scrolling === true) {
+      this.setState({ scrolling: false });
+    }
+    else if (window.scrollY !== 0 && this.state.scrolling !== true) {
+      this.setState({ scrolling: true });
+    }
+  }
+
   render() {
     return (
-      <div className="header-container">
+      // <div className="header-container">
+      <div className={`${this.state.scrolling ? 'header-container-scrolled' : 'header-container'}`}>
         <p className="header-logo">
           <span className="header-logo-yandex">
             <span className="header-logo-y">Y</span>andex
           </span>
-          <span>&nbsp;Taxi</span>
+          <span className="header-logo-taxi">&nbsp;Taxi</span>
         </p>
         <nav className="header-nav">
           <ul>
